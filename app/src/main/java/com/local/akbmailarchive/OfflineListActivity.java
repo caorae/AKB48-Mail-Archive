1b87844d71d2e99c388fa27edd4b4fc1181de675
Claire Cao <31811243+caorae@users.noreply.github.com>
2026-09-08 03:17:41 +0900

    Use resources from new namespace

--- app/src/main/java/com/local/akbmailarchive/OfflineListActivity.java
+++ app/src/main/java/com/local/akbmailarchive/OfflineListActivity.java
@@ -17,6 +17,7 @@ import android.widget.Spinner;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.yagidaisuki.akbmailarchive.R;
 import com.local.akbmailarchive.model.ArchivedMail;
 import com.local.akbmailarchive.storage.ArchiveDatabase;
 
