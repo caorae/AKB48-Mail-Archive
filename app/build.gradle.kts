6e25ae3e79d583e3a6373e00a76620b0e7b5fb8a
Claire Cao <31811243+caorae@users.noreply.github.com>
2026-09-08 03:44:41 +0900

    Set azudaisuki app namespace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.local.akbmailarchive"
+    namespace = "com.azudaisuki.akbmailarchive"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.local.akbmailarchive"
+        applicationId = "com.azudaisuki.akbmailarchive"
         minSdk = 26
         targetSdk = 35
         versionCode = 7
